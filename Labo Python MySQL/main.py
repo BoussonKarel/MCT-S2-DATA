b8e452fa329a932137e442ec8d28173dd90a4e4d
BoussonKarel <BoussonKarel@users.noreply.github.com>
2020-03-20 11:27:36 +0100

    Finished Labo

--- Labo Python MySQL/main.py
+++ Labo Python MySQL/main.py
@@ -1,18 +1,40 @@
-from repositories.CategorieRepository import CategorieRepository
+# from repositories.CategorieRepository import CategorieRepository
+# from repositories.KlantRepository import KlantRepository
 from services.CategorieServices import CategorieServices
+from services.KlantServices import KlantServices
 
 # CategorieServices.print_all_categories()
 # CategorieServices.print_details_categorie(5)
 # CategorieServices.print_details_categorie(912)
 # CategorieServices.print_details_categorie()
 
-choice = ""
-while(choice.upper() != "X"):
-    print("Kies een optie: R = Read, S = Search, X = Exit"
-    + "\n-----------------------------------")
-    choice = input("Jouw optie [R,S,X] = ")
-    if choice.upper() == "R":
-        CategorieServices.print_all_categories()
-    elif choice.upper() == "S":
-        categorienummer = int(input("Categorienummer: "))
-        CategorieServices.print_details_categorie(categorienummer)
+# print(KlantRepository.read_all())
+# print(KlantRepository.details(5))
+
+# KlantServices.print_all_klanten()
+# KlantServices.print_details_klant(5)
+
+def main():
+    print("Kies een optie:"
+    + "\n- RC = Read Categories"
+    + "\n- SC = Search Categories"
+    + "\n- RK = Read Klanten"
+    + "\n- SK = Search Klanten"
+    + "\n- X = Exit")
+    choice = ""
+
+    while(choice.upper() != "X"):
+        print("-----------------------------------")
+        choice = input("Jouw optie [RC, SC] of [RK, SK] of [X] = ")
+        if choice.upper() == "RC":
+            CategorieServices.print_all_categories()
+        elif choice.upper() == "SC":
+            categorienummer = int(input("Categorienummer: "))
+            CategorieServices.print_details_categorie(categorienummer)
+        elif choice.upper() == "RK":
+            KlantServices.print_all_klanten()
+        elif choice.upper() == "SK":
+            klantnummer = int(input("Klantnummer: "))
+            KlantServices.print_details_klant(klantnummer)
+
+main()
